58d4353e9fd36ed5dd24b6808dc2ae2debdbd42f
youngwzerox2 <dzzg34@gmail.com>
2023-12-06 18:08:15 +0900

    첫커밋2

---
+++ app.js
@@ -0,0 +1,34 @@
+let age = 32;
+let userName = 'Max';
+let hobbies = ['Sports', 'Cooking', 'Reading'];
+let job = { 
+    title: 'Developer', 
+    place: 'New York', 
+    salary: 50000 
+};
+
+let totaladultYears;
+
+function calculateAdultYears(userAge) {
+    let result;
+    result = userAge - 18;
+    return result;
+}
+
+totaladultYears = calculateAdultYears(age);
+console.log(totaladultYears);
+
+age = 45;
+totaladultYears = calculateAdultYears(age);
+
+console.log(totaladultYears);
+
+let person = {
+    name: 'Max', // Property
+    greet() {   // Method
+        console.log('Hello!');
+    }
+};
+
+person.greet();
+
