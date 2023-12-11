1a14ec8a16b6cd8651b79740cbf0124c056d68c5
Moon <ans738@naver.com>
2023-12-11 15:14:38 +0900

    문병준 commit

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 let age = 32;
-let userName = 'Max';
-let hobbies = ['Sports', 'Cooking', 'Reading'];
-let job = { 
-    title: 'Developer', 
-    place: 'New York', 
-    salary: 50000 
+let userName = "Max";
+let hobbies = ["Sports", "Cooking", "Reading"];
+let job = {
+  title: "Developer",
+  place: "New York",
+  salary: 50000,
 };
 
 let totaladultYears;
 
 function calculateAdultYears(userAge) {
-    let result;
-    result = userAge - 18;
-    return result;
+  let result;
+  result = userAge - 18;
+  return result;
 }
 
 totaladultYears = calculateAdultYears(age);
@@ -24,11 +24,11 @@ totaladultYears = calculateAdultYears(age);
 console.log(totaladultYears);
 
 let person = {
-    name: 'Max', // Property
-    greet() {   // Method
-        console.log('Hello!');
-    }
+  name: "Max", // Property
+  greet() {
+    // Method
+    console.log("Hello!");
+  },
 };
 
 person.greet();
-
