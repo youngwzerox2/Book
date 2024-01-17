bfbdd11cefc65239ee2a4406727161551925336f
LJH6227 <149341883+LJH6227@users.noreply.github.com>
2024-01-17 17:11:08 +0900

    초기 커밋 메시지

---
+++ adminbook/main/java/com/example/domain/Notice.java
@@ -0,0 +1,18 @@
+package com.example.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class Notice {
+
+	private Integer noticeNumber;		// 글 번호
+	private String	noticeType;			// 글 카테고리
+	private String	noticeTitle;		// 글 제목
+	private String	noticeContent;		// 글 내용
+	private Date	noticeDate;			// 글 등록일
+	private Integer noneDisplay;		// 글 공개여부
+	private String	memberId;			// 작성자
+	
+}
