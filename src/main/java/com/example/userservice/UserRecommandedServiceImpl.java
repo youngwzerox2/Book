6a7b62435e7aad167836a6e84011b200bad930e1
youngwzerox2 <dzzg34@gmail.com>
2024-01-21 10:14:38 +0900

    0121 1014 마스터커밋

--- src/main/java/com/example/userservice/UserRecommandedServiceImpl.java
+++ src/main/java/com/example/userservice/UserRecommandedServiceImpl.java
@@ -3,10 +3,12 @@ package com.example.userservice;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.example.domain.Recommanded;
 import com.example.userdao.UserRecommandedDAO;
 
+@Service
 public class UserRecommandedServiceImpl implements UserRecommandedService {
 
     @Autowired
